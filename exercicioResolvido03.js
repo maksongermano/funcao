6f1ebde54ae72dae17b897e2ef30e554c88cd68e
unknown <maksongermano@gmail.com>
2023-03-22 20:12:37 -0300

    apresentação customisada

--- exercicioResolvido03.js
+++ exercicioResolvido03.js
@@ -12,21 +12,22 @@ function determinarFaixaEtaria(idade) {
     } else if (idade < 100) {
         return 'idoso(a)'
     } else {
-        return 'hi is a vampire'
+        return 'Vampiro'
     }
+}
+function BoasVindas(pessoa) {
+    const faixaetaria = determinarFaixaEtaria(pessoa.idade)
+    console.log(`Olá! Meu nome é ${pessoa.nome}, sou um ${faixaetaria} de ${pessoa.idade} anos, tenho ${pessoa.altura}m e a minha ocupação é ser ${pessoa.profissao}`);
 }
 
 const pessoa01 = {
     nome: 'makson',
-    idade: 31,
+    idade: 101,
     altura: 1.83,
     profissao: "Estudante"
 
 }
 
-function BoasVindas(pessoa) {
-    const faixaetaria = determinarFaixaEtaria(pessoa.idade)
-    console.log(`Olá! Meu nome é ${pessoa.nome}, sou um jovem de ${pessoa.idade} anos, tenho ${pessoa.altura}m e a minha ocupação é ser ${pessoa.profissao}`);
-}
+
 
 BoasVindas(pessoa01);
