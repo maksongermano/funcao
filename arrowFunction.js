adea56629f5b7c4045891883c410453eab05f191
unknown <maksongermano@gmail.com>
2023-03-22 21:20:55 -0300

    outras maneiras de fazer função

---
+++ arrowFunction.js
@@ -0,0 +1,14 @@
+
+// declaração tradicional
+// function soma(x, y) {
+//     // codigo
+//     const resultasdo = x + y;
+//     return resultasdo;
+// }
+// declaração utilizando arrow-function
+const soma = (x, y) => {
+    // codigo
+    const resultado = x + y
+    return resultado
+}
+console.log(soma(10, 6));
---
+++ escopoDeFuncao.js
@@ -0,0 +1,8 @@
+
+
+function soma(x, y) {
+    let z = 5;
+    return x + y + z;
+}
+
+console.log(soma(5, 3)); // você não poderia exibir o z por que ela foi criada dentro do escopo da função
